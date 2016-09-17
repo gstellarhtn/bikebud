8e19e6d5e6f333247c8d1fae2d76cd0903dcfac4
Alex <alex@alisterdev.com>
2016-09-17 15:25:14 -0400

    abstract map into a service

--- app/www/js/controllers/MapCtrl.js
+++ app/www/js/controllers/MapCtrl.js
@@ -1,6 +1,9 @@
 angular.module('bb-app')
 
-  .controller('MapCtrl', function($scope, uiGmapGoogleMapApi, uiGmapIsReady, bixiStationService) {
+  .controller('MapCtrl', function($scope, uiGmapGoogleMapApi, uiGmapIsReady, bixiStationService, Map) {
+
+    bixiStationService.getTorontoBixi();
+
 
     $scope.map = {
       center: {
@@ -23,101 +26,13 @@ angular.module('bb-app')
         zoomControl: true
       }
     };
-
-    bixiStationService.getTorontoBixi();
-
-    // $scope.marker = {
-    //   id: 0,
-    //   options: {draggable: false},
-    //   events: {
-    //     click: function (marker) {
-    //       var markerCount = Map.getUserRoute().coordinates.length;
-    //       if (markerCount < 2) {
-    //         var lat = marker.getPosition().lat();
-    //         var lon = marker.getPosition().lng();
-    //         marker.setVisible(false);
-    //         Map.addMarker(new google.maps.LatLng(lat, lon));
-    //       }
-    //
-    //     }
-    //   }
-    // };
-
     uiGmapGoogleMapApi.then(function(maps) {
+
     });
 
     // fires when gmap is loaded
     uiGmapIsReady.promise(1).then(function (instances) {
        var inst = instances[0]; // gets the map
-       calculateAndDisplayRoute(inst.map);
+       Map.calculateAndDisplayRoute(inst.map);
      });
-
-    function calculateAndDisplayRoute(map) {
-
-        var directionsService = new google.maps.DirectionsService();
-     var directionsDisplay = new google.maps.DirectionsRenderer({preserveViewport: true, suppressMarkers: true});
-     directionsDisplay.setMap(map);
-
-        var waypts = [];
-        // var checkboxArray = document.getElementById('waypoints');
-        // for (var i = 0; i < checkboxArray.length; i++) {
-        //   if (checkboxArray.options[i].selected) {
-        //     waypts.push({
-        //       location: checkboxArray[i].value,
-        //       stopover: true
-        //     });
-        //   }
-        // }
-          waypts.push({
-            location: "montreal, quebec",
-            stopover: true
-          });
-          waypts.push({
-            location: "toronto, ont",
-            stopover: true
-          });
-
-        directionsService.route({
-          // origin: document.getElementById('start').value,
-          // destination: document.getElementById('end').value,
-          origin: "Halifax, NS",
-          destination: "Vancouver, BC",
-          waypoints: waypts,
-          optimizeWaypoints: true,
-          travelMode: 'BICYCLING'
-        }, function(response, status) {
-          if (status === 'OK') {
-
-            directionsDisplay.setMap(map)
-            directionsDisplay.setDirections(response);
-            // var route = response.routes[0];
-
-            // var summaryPanel = document.getElementById('directions-panel');
-            // summaryPanel.innerHTML = '';
-            // // For each route, display summary information.
-            // for (var i = 0; i < route.legs.length; i++) {
-            //   var routeSegment = i + 1;
-            //   summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
-            //       '</b><br>';
-            //   summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
-            //   summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
-            //   summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
-            // }
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-
-
-
-      }
-
-    // $scope.playlists = [
-    //   { title: 'Reggae', id: 1 },
-    //   { title: 'Chill', id: 2 },
-    //   { title: 'Dubstep', id: 3 },
-    //   { title: 'Indie', id: 4 },
-    //   { title: 'Rap', id: 5 },
-    //   { title: 'Cowbell', id: 6 }
-    // ];
   })
---
+++ app/www/js/factories/map.js
@@ -0,0 +1,86 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('bb-app')
+    .factory('Map', Map);
+
+  /**
+   * A factory that take care of all google map interactions
+   * @param $state
+   * @param $q
+   * @param $log
+   * @returns {{init: init, addMarker: addMarker, addRoutes: addRoutes, addCircles: addCircles, clearAvailableRoutes: clearAvailableRoutes, clearAllRoutes: clearAllRoutes, dispDirections: dispDirections, geocodeLocation: geocodeLocation, getUserRoute: getUserRoute, isLoadedFn: isLoadedFn, setLoaded: setLoaded}}
+   * @constructor
+   */
+  function Map($state, $q, $log) {
+    // Public API
+    var Map = {
+      calculateAndDisplayRoute: calculateAndDisplayRoute
+    };
+
+    return Map;
+
+    function calculateAndDisplayRoute(map) {
+
+      var directionsService = new google.maps.DirectionsService();
+      var directionsDisplay = new google.maps.DirectionsRenderer({preserveViewport: true, suppressMarkers: true});
+      directionsDisplay.setMap(map);
+
+      var waypts = [];
+      // var checkboxArray = document.getElementById('waypoints');
+      // for (var i = 0; i < checkboxArray.length; i++) {
+      //   if (checkboxArray.options[i].selected) {
+      //     waypts.push({
+      //       location: checkboxArray[i].value,
+      //       stopover: true
+      //     });
+      //   }
+      // }
+      waypts.push({
+        location: "montreal, quebec",
+        stopover: true
+      });
+      waypts.push({
+        location: "toronto, ont",
+        stopover: true
+      });
+
+      directionsService.route({
+        // origin: document.getElementById('start').value,
+        // destination: document.getElementById('end').value,
+        origin: "Halifax, NS",
+        destination: "Vancouver, BC",
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: 'BICYCLING'
+      }, function(response, status) {
+        if (status === 'OK') {
+
+          directionsDisplay.setMap(map)
+          directionsDisplay.setDirections(response);
+          // var route = response.routes[0];
+
+          // var summaryPanel = document.getElementById('directions-panel');
+          // summaryPanel.innerHTML = '';
+          // // For each route, display summary information.
+          // for (var i = 0; i < route.legs.length; i++) {
+          //   var routeSegment = i + 1;
+          //   summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+          //       '</b><br>';
+          //   summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+          //   summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+          //   summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+          // }
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+
+
+
+    }
+
+  }
+
+})();
